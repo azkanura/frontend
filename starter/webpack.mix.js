3843fb0d6ee6b68e456afc421591d3ee368f59f7
Azka Nurun Ala <azka@badr-interactive.com>
2017-02-23 08:20:13 +0700

    create new starter project

---
+++ starter/webpack.mix.js
@@ -0,0 +1,23 @@
+const { mix } = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');
+
+// var elixir = require('laravel-elixir');
+
+// elixir(function(mix){
+// 	mix.styles('bootstrap.css','public/assets/css/app.css');
+// 	mix.sass(['_variable.scss','app.scss'],'public/assets/css/app.css');
+// 	mix.scripts(['jquery-3.1.1.min.js','bootstrap.js','app.js'],'public/assets/js/app.js');
+// });
